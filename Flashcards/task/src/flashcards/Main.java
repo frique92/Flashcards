8ca95a623f205660781f908dce2d1a087db3aed9
frique92 <29735207+frique92@users.noreply.github.com>
2020-03-02 09:17:11 +0300

    complete stage 1 of course

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -2,6 +2,9 @@ package flashcards;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Card:");
+        System.out.println("purchase");
+        System.out.println("Definition:");
+        System.out.println("buy");
     }
 }
