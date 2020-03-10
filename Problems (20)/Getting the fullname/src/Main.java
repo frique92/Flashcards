e9b55437fc65ae16e2b6b8ced4aae3e2a5fa75af
frique92 <29735207+frique92@users.noreply.github.com>
2020-03-10 12:44:40 +0300

    complete any problems in the stage 4 of course

--- Problems (20)/Getting the fullname/src/Main.java
+++ Problems (20)/Getting the fullname/src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 class User {
     private String firstName;
     private String lastName;
---
+++ Problems (25)/Employee/src/Main.java
@@ -0,0 +1,24 @@
+class Employee {
+
+    String name;
+    int salary;
+    String address;
+
+    public Employee() {
+        this.name = "unknown";
+        this.address = "unknown";
+        this.salary = 0;
+    }
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.address = "unknown";
+        this.salary = salary;
+    }
+
+    public Employee(String name, int salary, String address) {
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+}
