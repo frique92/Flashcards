a2d01c29f2fc803f0ede176ec4b2137852faf828
frique92 <29735207+frique92@users.noreply.github.com>
2020-03-16 10:31:35 +0300

    complete "The Collections Framework overview" in the stage 4 of course

---
+++ Problems (48)/Printing strings in the same order/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.next();
+        System.out.println(word);
+        String word2 = scanner.next();
+        System.out.println(word2);
+        String word3 = scanner.next();
+        System.out.println(word3);
+        String word4 = scanner.next();
+        System.out.println(word4);
+
+    }
+}
---
+++ Problems (49)/Blacklist/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+class CollectionUtils {
+
+    public static Collection<String> filterPhones(Collection<String> phones,
+                                                  Collection<String> blacklist) {
+
+        Collection<String> cleanPhones = new ArrayList<>();
+
+
+        for (String phone :
+                phones) {
+
+            if (!blacklist.contains(phone)) cleanPhones.add(phone);
+
+        }
+
+        return cleanPhones;
+    }
+}
+
+/* Please, do not modify this I/O code */
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Collection<String> phones = Arrays.asList(scanner.nextLine().split("\\s+"));
+        Collection<String> blockList = Arrays.asList(scanner.nextLine().split("\\s+"));
+
+        Collection<String> nonBlockedPhones = CollectionUtils.filterPhones(
+                new ArrayList<>(phones), new ArrayList<>(blockList));
+
+        System.out.println(nonBlockedPhones.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(" ")));
+    }
+}
