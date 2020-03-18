4b376023b40cf95cbe00d3e97eb7969476b92433
frique92 <29735207+frique92@users.noreply.github.com>
2020-03-18 11:01:45 +0300

    complete stage 4

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -1,42 +1,57 @@
 package flashcards;
 
-import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Map<String, String> flashcards = new LinkedHashMap<>();
+
+        System.out.println("Input the number of cards:");
+
         int numbersOfCards = scanner.nextInt();
         scanner.nextLine();
 
-        String[] cards = new String[numbersOfCards];
-        String[] definitionCards = new String[numbersOfCards];
+        for (int i = 1; i <= numbersOfCards; i++) {
+            String name = "";
+            String definition = "";
+            boolean cardPresent = true;
+            boolean definitionPresent = true;
 
-        int count = 0;
-        for (int i = 0; i < numbersOfCards; i++) {
-            count++;
+            System.out.println("The card #" + i + ":");
+            while (cardPresent) {
+                name = scanner.nextLine();
+                if (flashcards.containsValue(name)) {
+                    System.out.println("The card \"" + name + "\" already exists. Try again:");
+                } else cardPresent = false;
+            }
 
-            System.out.println("The card #" + count + ":");
-            String name = scanner.nextLine();
-            System.out.println("The definition of the card #" + count + ":");
-            String definition = scanner.nextLine();
+            System.out.println("The definition of the card #" + i + ":");
+            while (definitionPresent) {
+                definition = scanner.nextLine();
+                if (flashcards.containsKey(definition)) {
+                    System.out.println("The definition \"" + definition + "\" already exists. Try again:");
+                } else definitionPresent = false;
+            }
 
-            cards[i] = name;
-            definitionCards[i] = definition;
+            flashcards.put(definition, name);
         }
 
-//        System.out.println(Arrays.toString(cards));
-//        System.out.println(Arrays.toString(definitionCards));
-
-        for (int i = 0; i < cards.length; i++) {
-            System.out.println("Print the definition of \"" + cards[i] + "\":");
+        for (Map.Entry<String, String> entry : flashcards.entrySet()) {
+            System.out.println("Print the definition of \"" + entry.getValue() + "\":");
             String definition = scanner.nextLine();
-            if (definition.equals(definitionCards[i])) {
-                System.out.println("Correct answer.");
-            } else {
-                System.out.println("Wrong answer. The correct one is \"" + definitionCards[i] + "\".");
-            }
+
+            if (entry.getKey().equals(definition)) System.out.println("Correct answer.");
+            else if (flashcards.containsKey(definition)) {
+                System.out.println("Wrong answer. The correct one is \""
+                        + entry.getKey() + "\", you've just written the definition of \""
+                        + flashcards.get(definition) + "\".");
+            } else System.out.println("Wrong answer. The correct one is \"" + entry.getKey() + "\".");
+
         }
 
     }
